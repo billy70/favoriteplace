78b77c885eb2ec5c1ee3fbe3f5189fa3b37f08ad
William L. Marr III <hobbyist@wlmiii.net>
2016-04-01 16:01:23 -0400

    Give the two buttons nice rounded corners

--- FavoritePlace/ViewController.swift
+++ FavoritePlace/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var learnMoreButton: UIButton!
+    @IBOutlet weak var photosButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        learnMoreButton.layer.cornerRadius = 6.0
+        photosButton.layer.cornerRadius = 6.0
     }
 
     override func didReceiveMemoryWarning() {
