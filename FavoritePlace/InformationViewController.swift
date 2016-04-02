7608eaa5924c463d11d1b1539b9fe7982ac049e2
William L. Marr III <hobbyist@wlmiii.net>
2016-04-01 21:10:45 -0400

    Add view controller class for the information scene

---
+++ FavoritePlace/InformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InformationViewController.swift
+//  FavoritePlace
+//
+//  Created by William L. Marr III on 4/1/16.
+//  Copyright © 2016 William L. Marr III. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        backButton.layer.cornerRadius = 6.0
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backButtonTapped(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
