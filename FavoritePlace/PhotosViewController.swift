84caee4a6aeeba2e4d84a4162fa89aeefa01a560
William L. Marr III <hobbyist@wlmiii.net>
2016-04-02 11:05:19 -0400

    Refine the photos layout and make 'back' button dismiss the scene

--- FavoritePlace/PhotosViewController.swift
+++ FavoritePlace/PhotosViewController.swift
@@ -10,26 +10,21 @@ import UIKit
 
 class PhotosViewController: UIViewController {
 
+    @IBOutlet weak var backButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        backButton.layer.cornerRadius = 6.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonTapped(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
 }
